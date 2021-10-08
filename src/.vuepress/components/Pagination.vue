ee7fc8ee35ef896bf9584fc523006c6cb879900e
Daulet <anakhaev07@gmail.com>
2021-10-08 14:06:36 +0300

    ADD: Helper utils, Pagination range

--- src/.vuepress/components/Pagination.vue
+++ src/.vuepress/components/Pagination.vue
@@ -3,6 +3,31 @@
 </template>
 <script>
 export default {
+    name: 'Pagination',
+    props: {
+        total: {
+            type: Number,
+            required: true
+        },
+        limit: {
+            type: Number,
+            required: true
+        },
+        currenPage: {
+            type: Number,
+            required: true
+        },
+        url: {
+            type: String,
+            required: true
+        },
+    },
+    computed: {
+        pages() {
+            const pagesCount = this.total / this.limit;
+            
+        }
+    }
     
 }
 </script>
